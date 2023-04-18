4e56c88a45e0d393ebac0a6c04e291e5676442f7
Tianjian Jiang <jiangtj13@gmail.com>
2023-04-18 09:43:41 +0200

    Add instruction to install all the dependencies

---
+++ install.sh
@@ -0,0 +1,11 @@
+pip install torch==1.13.1+cu116 torchvision==0.14.1 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu116_pyt1131/download.html
+# pip install git+https://github.com/NVlabs/tiny-cuda-nn/@v1.6#subdirectory=bindings/torch
+pip install pytorch-lightning==1.5.7
+pip install torch-ema
+pip install opencv-python
+pip install imageio
+pip install scipy==1.10.1
+pip install smplx==0.1.28
+pip install torchmetrics==0.11.4
+pip install lpips==0.1.4
